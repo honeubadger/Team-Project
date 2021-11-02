6eaf2e0c4f35a9010da00cb2685448d9c9151458
edwardangeles <edward.angelesm@gmail.com>
2021-11-02 19:36:19 +0000

    dev

--- app/src/main/java/com/byui/teamproject/MainActivity.java
+++ app/src/main/java/com/byui/teamproject/MainActivity.java
@@ -2,7 +2,11 @@ package com.byui.teamproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.byui.teamproject.startshift.StartShiftActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onStartShiftClicked(View view) {
+        Intent intent = new Intent(this, StartShiftActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/byui/teamproject/startshift/StartShiftActivity.java
@@ -0,0 +1,16 @@
+package com.byui.teamproject.startshift;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.byui.teamproject.R;
+
+public class StartShiftActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_shift);
+    }
+}
