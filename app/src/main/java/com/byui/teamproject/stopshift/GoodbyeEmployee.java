91704bf55be8056b60c910bd314e01fc83d71a3e
Nana <nananzama@yahoo.com>
2021-11-12 21:19:40 +0200

    Stop Shift Activities Created

---
+++ app/src/main/java/com/byui/teamproject/stopshift/GoodbyeEmployee.java
@@ -0,0 +1,16 @@
+package com.byui.teamproject.stopshift;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.byui.teamproject.R;
+
+public class GoodbyeEmployee extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_goodbye_employee);
+    }
+}
---
+++ app/src/main/java/com/byui/teamproject/stopshift/LogoutWithFaceID.java
@@ -0,0 +1,16 @@
+package com.byui.teamproject.stopshift;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.byui.teamproject.R;
+
+public class LogoutWithFaceID extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logout_with_face_id);
+    }
+}
